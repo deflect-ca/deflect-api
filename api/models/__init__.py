8bfbea4fedd41c956b4adf788ecbf242cd1e6d0f
Jeremy Yen <jeremy5189@gmail.com>
2020-09-25 00:52:15 +0800

    Create cert model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -4,3 +4,4 @@ Import database models so ORM can create the database
 from .website import Website
 from .website_option import WebsiteOption
 from .record import Record
+from .certificate import Certificate
---
+++ api/models/certificate.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Certificate(models.Model):
+    class Meta:
+        app_label = "api"
+
+    # id = db.Column(db.Integer, primary_key=True)
+    id = models.AutoField(primary_key=True)
+
+    # hostnames = db.Column(db.Text(65000), nullable=False)
+    hostnames = models.TextField(null=False)
+
+    # fingerprint = db.Column(db.String(255), nullable=False)
+    fingerprint = models.CharField(max_length=255, null=False)
+
+    # has_private = db.Column(db.Boolean, default=False)
+    has_private = models.BooleanField(default=False, null=True, blank=True)
+
+    # The fingerprint of the issuing CA
+    # issuing_ca = db.Column(db.String(255), nullable=False)
+    issuing_ca = models.CharField(max_length=255, null=False)
+
+    # date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    # date_expires = db.Column(db.DateTime)
+    date_expires = models.DateTimeField(null=True, blank=True)
+
+    # one-to-many with website
+    # access website.certificates to get all options
+    website = models.ForeignKey('Website',
+        on_delete=models.CASCADE, related_name='certificates')
+
+    def __repr__(self):
+        return '<Certificate hostnames={}>'.format(self.hostnames.split(", "))
