f50af659e1d01fee46c54f400b0cc643e6c01fde
Jeremy Yen <jeremy5189@gmail.com>
2020-10-01 01:49:46 +0800

    Register model admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Website, WebsiteOption
+from .models import Website, WebsiteOption, Record, Certificate, YamlDiff
 
 # Register your models here.
 admin.site.register(Website)
 admin.site.register(WebsiteOption)
+admin.site.register(Record)
+admin.site.register(Certificate)
+admin.site.register(YamlDiff)
