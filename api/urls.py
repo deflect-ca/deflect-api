e1712348be5732741651e542a8bd5a5f46dc995a
Jeremy Yen <jeremy5189@gmail.com>
2020-10-06 14:52:44 +0800

    Restruct view dir

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import views
+from .views import edgemanage_view
 
 urlpatterns = [
-    path('info', views.api_info, name='api_info'),
-    path('edge/list', views.api_edge_query, name='api_edge_query'),
-    path('edge/dnet', views.api_dnet_query, name='api_dnet_query'),
-    path('edge/config', views.api_edge_conf, name='api_edge_conf'),
+    path('info', edgemanage_view.api_info, name='api_info'),
+    path('edge/list', edgemanage_view.api_edge_query, name='api_edge_query'),
+    path('edge/dnet', edgemanage_view.api_dnet_query, name='api_dnet_query'),
+    path('edge/config', edgemanage_view.api_edge_conf, name='api_edge_conf'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views/edgemanage_view.py
+++ api/views/edgemanage_view.py
@@ -1,12 +1,13 @@
-import django
 import logging
+import django
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
 
-from .modules.edgemanage import edge_query, edge_conf, dnet_query
+from api.modules.edgemanage import edge_query, edge_conf, dnet_query
+
 
 @api_view(['GET'])
 def api_info(request):
