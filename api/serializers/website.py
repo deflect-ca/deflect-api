c5787f955201904d703882157d192ab38ed5f256
Jeremy Yen <jeremy5189@gmail.com>
2020-10-14 16:47:36 +0800

    Add DB transaction for create/update

--- api/serializers/website.py
+++ api/serializers/website.py
@@ -1,6 +1,7 @@
 import logging
 import marshmallow
 
+from django.db import transaction
 from rest_framework import serializers
 from api.models import Website, WebsiteOption
 from .website_options import WebsiteOptionSerializer
@@ -30,29 +31,37 @@ class WebsiteSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
     # Writable nested serializers
+    @transaction.atomic
     def create(self, validated_data):
         options = validated_data.pop('options')
         website = Website.objects.create(**validated_data)
+
         for option in options:
             try:
                 # Validate and create
                 website.set_option(option['name'], option['data'])
             except marshmallow.ValidationError as err:
-                website.delete()
+                # invoke transaction rollback
                 raise serializers.ValidationError(str(err))
+
         return website
 
     # Updatable nested serializers
+    @transaction.atomic
     def update(self, instance, validated_data):
         # pop options
         options = validated_data.pop('options')
+
         # call parent update function
         updated_instance = super(WebsiteSerializer, self).update(
             instance, validated_data)
+
         for option in options:
             try:
                 # validate and create or update
                 updated_instance.set_option(option['name'], option['data'])
             except marshmallow.ValidationError as err:
+                # invoke transaction rollback
                 raise serializers.ValidationError(str(err))
+
         return instance
