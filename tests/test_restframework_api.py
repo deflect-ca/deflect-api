f6715e908fb09280cf57d1061c05628b1350fcfd
Jeremy Yen <jeremy5189@gmail.com>
2020-10-15 17:45:59 +0800

    Add test for modify website API

--- tests/test_restframework_api.py
+++ tests/test_restframework_api.py
@@ -28,6 +28,7 @@ class RestFrameworkTestCase(TestCase):
             "ip_address": "127.0.0.1",
             "admin_key": "/wp-admin"
         })
+
         obj = response.json()
         self.assertEqual(response.status_code, 201)
         self.assertEqual(obj["url"], "test.com")
@@ -52,7 +53,182 @@ class RestFrameworkTestCase(TestCase):
             "admin_key": "/wp-admin",
             "options": options
         }, format='json')
+
         obj = response.json()
         self.assertEqual(response.status_code, 201)
         self.assertEqual(obj["url"], "test2.com")
         self.assertEqual(obj["options"], options)
+
+    def test_04_website_create_with_option_error(self):
+        # option_not_existed
+        options = [
+            {
+                "name": "option_not_existed",
+                "data": True
+            }
+        ]
+        response = self.client.post("/api/website/create", {
+            "url": "test3.com",
+            "status": 0,
+            "ip_address": "127.0.0.1",
+            "admin_key": "/wp-admin",
+            "options": options
+        }, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Unknown field", response.json()[0])
+
+        # invalid data type for option
+        options = [
+            {
+                "name": "cache_time",  # should be integer
+                "data": "not integer"
+            }
+        ]
+        response = self.client.post("/api/website/create", {
+            "url": "test3.com",  # also make sure if DB rollback is working
+            "status": 0,
+            "ip_address": "127.0.0.1",
+            "admin_key": "/wp-admin",
+            "options": options
+        }, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Not a valid integer", response.json()[0])
+
+        # invalid field name
+        options = [
+            {
+                "name": "cache_time",  # should be integer
+                "value": 20
+            }
+        ]
+        response = self.client.post("/api/website/create", {
+            "url": "test3.com",
+            "status": 0,
+            "ip_address": "127.0.0.1",
+            "admin_key": "/wp-admin",
+            "options": options
+        }, format='json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("KeyError", response.json()[0])
+
+    def test_05_website_detail(self):
+        response = self.client.get("/api/website/1")
+        self.assertEqual(response.status_code, 200)
+
+        obj = response.json()
+        self.assertEqual(obj['url'], 'example.com')
+        self.assertEqual(obj['ip_address'], '192.0.2.0')  # later test modify in 06
+
+        options = [
+            {
+                "name": "use_ssl",
+                "data": False
+            },
+            {
+                "name": "cache_time",
+                "data": 10
+            }
+        ]
+        self.assertEqual(obj['options'], options)
+
+    def test_06_website_modify_without_options(self):
+        # test partial update
+        response = self.client.put("/api/website/1/modify", {
+            "ip_address": "127.0.0.1"
+        }, format='json')
+
+        obj = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(obj['ip_address'], "127.0.0.1")
+
+    def test_07_website_modify_with_options(self):
+        # Add 1 option, without modify anything
+        response = self.client.put("/api/website/1/modify", {
+            "options": [
+                {
+                    "name": "use_custom_ssl",
+                    "data": False
+                }
+            ]
+        }, format='json')
+
+        obj = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(obj['options'], [
+            {
+                "name": "use_ssl",
+                "data": False
+            },
+            {
+                "name": "cache_time",
+                "data": 10
+            },
+            {
+                "name": "use_custom_ssl",
+                "data": False
+            }
+        ])
+
+        # Modify 2 options
+        response = self.client.put("/api/website/1/modify", {
+            "options": [
+                {
+                    "name": "use_ssl",
+                    "data": True
+                },
+                {
+                    "name": "cache_time",
+                    "data": 87
+                },
+                # no use_custom_ssl, should left unchanged
+            ]
+        }, format='json')
+
+        obj = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(obj['options'], [
+            {
+                "name": "use_ssl",
+                "data": True
+            },
+            {
+                "name": "cache_time",
+                "data": 87
+            },
+            {
+                "name": "use_custom_ssl",
+                "data": False
+            }
+        ])
+
+    def test_08_website_modify_with_options_and_err(self):
+        # option_not_existed
+        response = self.client.put("/api/website/1/modify", {
+            "options": [
+                {
+                    "name": "option_not_existed",
+                    "data": False
+                }
+            ]
+        }, format='json')
+
+        obj = response.json()
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Unknown field", response.json()[0])
+
+        # field invalid
+        response = self.client.put("/api/website/1/modify", {
+            "options": [
+                {
+                    "name": "cache_time",
+                    "value": False
+                }
+            ]
+        }, format='json')
+
+        obj = response.json()
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("KeyError", response.json()[0])
