8104f0ed6880ad315d82d42a4f6e570e015a9338
Jeremy Yen <jeremy5189@gmail.com>
2021-02-22 23:18:45 +0800

    Change api doc theme

--- docs/gen-docs.sh
+++ docs/gen-docs.sh
@@ -1,3 +1,3 @@
 python manage.py generateschema --title 'Deflect Core API Documentation' --format openapi-json > docs/schema.json
 python manage.py docs_proc -i docs/schema.json -o docs/schema-altered.json
-apistar docs --path docs/schema-altered.json --format openapi --theme apistar --serve
+apistar docs --path docs/schema-altered.json --format openapi --theme swaggerui --serve
