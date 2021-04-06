8d7c425005e5fc79a7e31f11ec561b70402a63df
Jeremy Yen <jeremy5189@gmail.com>
2021-04-06 18:45:16 +0800

    Update

--- api/management/commands/deflect_next.py
+++ api/management/commands/deflect_next.py
@@ -9,11 +9,11 @@ import ssh_agent_setup
 from django.core.management.base import BaseCommand
 from deflect_next.orchestration import old_to_new_site_dict
 from deflect_next.orchestration import install_delta_config
-
 from deflect_next.orchestration import generate_bind_config
 from deflect_next.orchestration import decrypt_and_verify_cert_bundles
 from deflect_next.orchestration import generate_nginx_config
 from deflect_next.orchestration import generate_banjax_next_config
+import deflect_next.orchestration.conf as deflect_next_conf
 
 logger = logging.getLogger(__name__)
 
@@ -64,6 +64,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        config = {}
+        with open(options['config'], 'r') as file_config:
+            config = yaml.load(file_config.read(), Loader=yaml.FullLoader)
+
         old_sites_yml = {}
         with open(options['sites'], "r") as file_sites:
             old_sites_yml = yaml.load(file_sites.read(), Loader=yaml.FullLoader)
@@ -74,42 +78,40 @@ class Command(BaseCommand):
         formatted_time = time.strftime("%Y-%m-%d_%H:%M:%S")
         output_dir = f"{options['output']}/{formatted_time}"
 
+        # Load the config and output_prefix into memory
+        deflect_next_conf.init(config, output_dir, False)
+
         if not os.path.isdir(output_dir):
             logger.info(f"mkdir {output_dir}")
             os.mkdir(output_dir)
 
         logger.info('old_to_new_site_dict')
         new_client_sites = old_to_new_site_dict.main(
-            old_sites, old_sites_timestamp, output_prefix=output_dir)
+            old_sites, old_sites_timestamp)
 
         system_sites = {}
         with open(options['sys'], "r") as f:
             system_sites = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-        config = {}
-        with open(options['config'], 'r') as file_config:
-            config = yaml.load(file_config.read(), Loader=yaml.FullLoader)
-
         all_sites = {'client': new_client_sites, 'system': system_sites}
         logger.debug(json.dumps(all_sites, indent=2))
 
         logger.info('generate_bind_config')
-        generate_bind_config.main(config, all_sites, formatted_time, output_prefix=output_dir)
+        generate_bind_config.main(config, all_sites, formatted_time)
 
         logger.info('decrypt_and_verify_cert_bundles')
-        decrypt_and_verify_cert_bundles.main(all_sites, formatted_time, config, output_prefix=output_dir)
+        decrypt_and_verify_cert_bundles.main(all_sites, formatted_time)
 
         logger.info('generate_nginx_config')
-        generate_nginx_config.main(all_sites, config, formatted_time, output_prefix=output_dir)
+        generate_nginx_config.main(all_sites, config, formatted_time)
 
         logger.info('generate_banjax_next_config')
-        generate_banjax_next_config.main(
-            config, all_sites, formatted_time, output_prefix=output_dir)
+        generate_banjax_next_config.main(config, all_sites, formatted_time)
 
         if not options['genonly']:
             logger.info(f"load ssh key from: {options['key']}")
             self.load_ssh_key(options['key'])
             logger.info('install_delta_config')
-            install_delta_config.main(config, all_sites, formatted_time, formatted_time, output_prefix=output_dir)
+            install_delta_config.main(config, all_sites, formatted_time, formatted_time)
         else:
             logger.info('genonly presented, ending successfully')
