45f62e92134cf761b04bb9885b04903d7bbacb64
Jeremy Yen <jeremy5189@gmail.com>
2021-04-27 01:29:39 +0800

    Add filter and relation in edge api

--- api/serializers/edge.py
+++ api/serializers/edge.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from api.models import Edge
+from api.serializers import DnetSerializer
 
 
 class EdgeSerializer(serializers.ModelSerializer):
@@ -7,3 +8,6 @@ class EdgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Edge
         fields = ['id', 'hostname', 'ip', 'dnet', 'created_at', 'updated_at']
+
+    # nested relations
+    dnet = DnetSerializer(many=False)
--- api/views/edge.py
+++ api/views/edge.py
@@ -1,4 +1,4 @@
-from api.models import Edge
+from api.models import Edge, Dnet
 from api.serializers import EdgeSerializer
 from api.modules.util import CustomSchema
 from rest_framework import generics
@@ -8,6 +8,14 @@ class EdgeList(generics.ListCreateAPIView):
     schema = CustomSchema(tags=['edge'])
     queryset = Edge.objects.all()
     serializer_class = EdgeSerializer
+
+    def get_queryset(self):
+        queryset = Edge.objects.all()
+        dnet_name = self.request.query_params.get('dnet')
+        dnet_inst = Dnet.objects.filter(name=dnet_name).first()
+        if dnet_inst is not None:
+            queryset = queryset.filter(dnet=dnet_inst)
+        return queryset
 
 
 class EdgeDetail(generics.RetrieveUpdateDestroyAPIView):
