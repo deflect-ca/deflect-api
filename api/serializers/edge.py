000042eb12c13e498895214afc600ee772e519fb
Jeremy Yen <jeremy5189@gmail.com>
2021-04-29 15:18:59 +0800

    Remove nested relation

--- api/serializers/edge.py
+++ api/serializers/edge.py
@@ -7,7 +7,5 @@ class EdgeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Edge
-        fields = ['id', 'hostname', 'ip', 'dnet', 'created_at', 'updated_at']
-
-    # nested relations
-    dnet = DnetSerializer(many=False)
+        fields = ['id', 'hostname', 'ip', 'created_at', 'updated_at']
+        read_only_fields = ['id']
